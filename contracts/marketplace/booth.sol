03433c6f0755d4786044f61c95956b61116aae97
SoloPython <mail@solopython.com>
2023-02-27 12:20:28 -0500

    NFT Marketplace

---
+++ contracts/marketplace/booth.sol
@@ -0,0 +1,106 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
+
+contract Booth is ReentrancyGuard {
+    // Variables
+    address payable public immutable feeAccount; // the account that receives fees
+    uint public immutable feePercent; // the fee percentage on sales 
+    uint public itemCount; 
+
+    struct Item {
+        uint itemId;
+        IERC1155 ticket;
+        uint tokenId;
+        uint price;
+        uint amount;
+        address payable seller;
+        bool sold;
+    }
+
+    // itemId -> Item
+    mapping(uint => Item) public items;
+
+    event Offered(
+        uint itemId,
+        address indexed ticket,
+        uint tokenId,
+        uint price,
+        uint amount,
+        address indexed seller
+    );
+
+    event Bought(
+        uint itemId,
+        address indexed ticket,
+        uint tokenId,
+        uint price,
+        uint amount,
+        address indexed seller,
+        address indexed buyer
+    );
+
+    constructor(uint _feePercent) {
+        feeAccount = payable(msg.sender);
+        feePercent = _feePercent;
+    }
+
+    // Make item to offer on the marketplace
+    function makeItem(IERC1155 _ticket, uint _tokenId, uint _price, uint _amount) external nonReentrant {
+        require(_price > 0, "Price must be greater than zero");
+        // increment itemCount
+        itemCount ++;
+        // transfer Ticket (NFT)
+        _ticket.safeTransferFrom(msg.sender, address(this), _tokenId, _amount, "");
+        // add new item to items mapping
+        items[itemCount] = Item (
+            itemCount,
+            _ticket,
+            _tokenId,
+            _price,
+            _amount,
+            payable(msg.sender),
+            false
+        );
+        // emit Offered event
+        emit Offered(
+            itemCount,
+            address(_ticket),
+            _tokenId,
+            _price,
+            _amount,
+            msg.sender
+        );
+    }
+
+    function purchaseItem(uint _itemId) external payable nonReentrant {
+        uint _totalPrice = getTotalPrice(_itemId);
+        Item storage item = items[_itemId];
+        require(_itemId > 0 && _itemId <= itemCount, "item doesn't exist");
+        require(msg.value >= _totalPrice, "not enough ether to cover item price and market fee");
+        require(!item.sold, "item already sold");
+        // pay seller and feeAccount
+        item.seller.transfer(item.price);
+        feeAccount.transfer(_totalPrice - item.price);
+        // update item to sold
+        item.sold = true;
+        // transfer nft to buyer
+        item.ticket.safeTransferFrom(address(this), msg.sender, item.tokenId, 1, "");
+        // emit Bought event
+        emit Bought(
+            _itemId,
+            address(item.ticket),
+            item.tokenId,
+            item.price,
+            1,
+            item.seller,
+            msg.sender
+        );
+    }
+
+    function getTotalPrice(uint _itemId) view public returns(uint){
+        return((items[_itemId].price*(100 + feePercent))/100);
+    }
+}
---
+++ contracts/marketplace/ticket.sol
@@ -0,0 +1,109 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/finance/PaymentSplitter.sol";
+import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Supply.sol";
+
+/// @custom:security-contact security@boomslag.com
+contract Ticket is ERC1155, AccessControl, Pausable, ERC1155Supply, PaymentSplitter {
+    bytes32 public constant URI_SETTER_ROLE = keccak256("URI_SETTER_ROLE");
+    bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    bytes32 public constant STK_ROLE = keccak256("STK_ROLE");
+
+    uint256 public mintPrice = 0.01 ether;
+    uint256 public stkMintPrice = 0.001 ether;
+    uint256 public currentSupply;
+    uint256 public ticketSupply;
+
+    uint256 public platformShare = 10;
+    uint256 public sellerShare = 90;
+
+    constructor(
+        address[] memory _payees,
+        uint256[] memory _shares
+    ) 
+    ERC1155("https://boomslag.com/courses/{id}")
+    PaymentSplitter(_payees, _shares)
+    {
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _grantRole(URI_SETTER_ROLE, msg.sender);
+        _grantRole(PAUSER_ROLE, msg.sender);
+        _grantRole(MINTER_ROLE, msg.sender);
+    }
+
+    event Mint(uint256 indexed id, uint256 qty);
+    event SetUri(string newuri);
+    event Stop();
+    event Start();
+
+    function setURI(string memory newuri) public onlyRole(URI_SETTER_ROLE) {
+        _setURI(newuri);
+        emit SetUri(newuri);
+    }
+
+    function pause() public onlyRole(PAUSER_ROLE) {
+        _pause();
+        emit Stop();
+    }
+
+    function unpause() public onlyRole(PAUSER_ROLE) {
+        _unpause();
+        emit Start();
+    }
+
+    function stkMint(uint256 id, uint256 qty)
+        public
+        payable
+        onlyRole(STK_ROLE)
+    {
+        require(msg.value >= stkMintPrice * qty, "Not Enough ETH to Create NFT");
+        require(totalSupply(id) + qty <= ticketSupply, "NFT Out of Stock");
+        emit Mint(id, qty);
+        _mint(msg.sender, id, qty, "");
+    }
+    
+    function mint(uint256 id, uint256 qty)
+        public
+        payable
+    {
+        require(msg.value >= mintPrice * qty, "Not Enough ETH to Create NFT");
+        require(totalSupply(id) + qty <= ticketSupply, "NFT Out of Stock");
+        emit Mint(id, qty);
+        _mint(msg.sender, id, qty, "");
+    }
+
+    function uri(uint256 _id) public view virtual override returns (string memory) {
+        require(exists(_id),"URI: Token does not exist.");
+        return string(abi.encodePacked(super.uri(_id),Strings.toString(_id), ".json" ));
+    }
+
+    function mintBatch(address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data)
+        public
+        onlyRole(MINTER_ROLE)
+    {
+        _mintBatch(to, ids, amounts, data);
+    }
+
+    function _beforeTokenTransfer(address operator, address from, address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data)
+        internal
+        whenNotPaused
+        override(ERC1155, ERC1155Supply)
+    {
+        super._beforeTokenTransfer(operator, from, to, ids, amounts, data);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC1155, AccessControl)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+}
